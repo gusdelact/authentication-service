fcfe377ecfd7a5fca4b5db3360eaf9d344de7992
Gustavo De la Cruz Tovar <gusdelact@Gustavo-De-la-Cruz-Tovars-MacBook.local>
2018-05-28 03:09:23 -0500

    se agrego configuracion para usar Sleuth y Zipkin

--- src/main/java/com/gfi/microservicios/InspectHeaderFilter.java
+++ src/main/java/com/gfi/microservicios/InspectHeaderFilter.java
@@ -1,5 +1,7 @@
 package com.gfi.microservicios;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.Filter;
@@ -13,13 +15,15 @@ import java.io.IOException;
 
 @Component
 public class InspectHeaderFilter implements Filter {
+	
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
             throws IOException, ServletException {
 
 
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
-        System.out.println("I AM HITTING THE AUTH SERVER: " + httpServletRequest.getHeader("Authorization"));
+        logger.info("I AM HITTING THE AUTH SERVER: " + httpServletRequest.getHeader("Authorization"));
 
         filterChain.doFilter(httpServletRequest, servletResponse);
     }
