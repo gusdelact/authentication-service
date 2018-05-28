6c4a8b48c69c7c1c6e30c4c4ac14e3b1e8291366
EC2 Default User <ec2-user@ip-172-31-14-212.us-west-2.compute.internal>
2018-05-28 18:19:57 +0000

    modificaciones a scripts de curl

--- cURLTest/invokeRouteBaseDummyMensaje.sh
+++ cURLTest/invokeRouteBaseDummyMensaje.sh
@@ -1,3 +1,3 @@
 curl -X GET \
   http://localhost:5555/api/microservicio-base/microservicio/v3/mensaje \
-  -H 'authorization: Bearer 2d5de33e-0cad-4d57-9328-621a93886468'
+  -H 'authorization: Bearer f3378722-0e00-451a-8c10-c3763b4fc00d'
